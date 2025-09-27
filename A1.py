114e2d85b19645a283e30d86d3b87076c045b01d
mahe-f <fmaahekaan@gmail.com>
2025-09-27 19:15:36 +0500

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,3 @@
+print(" \n hi there! \n ")    #\n means inserting a newline
+print("My birthday is on " ,2+3)
+print("I got" , 10000, end=" rps")  #"#end" means adding a character,value,word at the end of a phrase
---
+++ A2.py
@@ -0,0 +1,7 @@
+name=input("What is your Name?")
+print("hi!" ,name)
+city="Quetta"
+country="Pakistan"
+print(city , country)
+height= 48.7
+print("height=" , height , type(height))  #type() ,built-in function used to determine the type of an object.
